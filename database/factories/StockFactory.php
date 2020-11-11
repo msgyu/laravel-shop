46aaf5b72f7b78491b526763ba12d7a673d4f2be
nousi <nousidayo@gmail.com>
2020-11-11 15:30:57 +0900

    RUN "php artisan make:model Models/stock -a"

---
+++ database/factories/StockFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\stock;
+use Faker\Generator as Faker;
+
+$factory->define(stock::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
