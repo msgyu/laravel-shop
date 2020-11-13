aa93be0e3864c52f5ed3316ba8221eaf4702d6bc
nousi <nousidayo@gmail.com>
2020-11-13 22:47:02 +0900

    Add deleteCart

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -15,18 +15,31 @@ class ShopController extends Controller
         $stocks = Stock::Paginate(6);
         return view('shop', compact('stocks'));
     }
+
     public function myCart(Cart $cart)
     {
         $data = $cart->showCart();
         return view('mycart', $data);
     }
-    public function addMycart(Request $request,Cart $cart)
+
+    public function addMycart(Request $request, Cart $cart)
     {
         $stock_id = $request->stock_id;
         $message = $cart->addCart($stock_id);
 
         $data = $cart->showCart();
 
-       return view('mycart',$data)->with('message',$message)
+        return view('mycart', $data)->with('message', $message);
+    }
+
+    public function deleteCart(Request $request, Cart $cart)
+    {
+
+        $stock_id = $request->stock_id;
+        $message = $cart->deleteCart($stock_id);
+
+        $data = $cart->showCart();
+
+        return view('mycart', $data)->with('message', $message);
     }
 }
