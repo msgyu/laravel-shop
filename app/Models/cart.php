4bb75895a47128809a4db8496ed613dde7eecbb7
nousi <nousidayo@gmail.com>
2020-11-13 22:03:29 +0900

    Add ShowCart

--- app/Models/cart.php
+++ app/Models/cart.php
@@ -3,10 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class cart extends Model
 {
     protected $fillable = [
         'stock_id', 'user_id',
     ];
+
+    public function showCart()
+    {
+        $user_id = Auth::id();
+        return $this->where('user_id', $user_id)->get();
+    }
 }
