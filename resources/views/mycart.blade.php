7c79d49a9a497f344091c35ee1192faba7bfaf32
nousi <nousidayo@gmail.com>
2020-11-13 09:45:46 +0900

    user nameを追加　in mycart.blade.php

--- resources/views/mycart.blade.php
+++ resources/views/mycart.blade.php
@@ -5,13 +5,14 @@
   <div class="">
     <div class="mx-auto" style="max-width:1200px">
       <h1 class="text-center font-weight-bold" style="color:#555555;
-             font-size:1.2em; padding:24px 0px;">カートの中身</h1>
+             font-size:1.2em; padding:24px 0px;">{{ Auth::user()->name }}さんのカートの中身</h1>
       <div class="card-body">
         @foreach($my_carts as $my_cart)
         {{$my_cart->stock_id}}<br>
         {{$my_cart->user_id}}<br>
         @endforeach
       </div>
+      <a href="/">商品一覧へ</a>
     </div>
   </div>
 </div>
